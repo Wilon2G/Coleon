89d75798725f88c4d882d5750bf63161ca38fc9c
Guillermo <gleon@druidsoftware.com>
2025-04-02 21:29:17 +0100

    Start of proyect with Django, first steps

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# Views are not really what a regular developer would think
+# In python they act more like request handelers
+# What we would call a view in react in python is called a template
+
+def say_hello(request):
+    
+    return render(request, 'hello.html',{"name":"liquen"})
